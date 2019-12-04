4d39f9f255795939be59a1010efaeff6c9793481
Melissa Hazlewood <meli.hazlewood@gmail.com>
2019-12-03 18:32:14 -0800

    Initial Commit

---
+++ Lab6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab6
+//
+//  Created by Melissa Hazlewood on 12/3/19.
+//  Copyright © 2019 Melissa Hazlewood. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
